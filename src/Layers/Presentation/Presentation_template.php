b8fe01d89cbf6a6ef0eb998fdf48884f8f2845f4
BartRoosen <bart.roosen@live.be>
2016-09-29 08:57:03 +0200

    html head addition

--- src/Layers/Presentation/Presentation_template.php
+++ src/Layers/Presentation/Presentation_template.php
@@ -4,6 +4,8 @@
     <head>
         <meta charset="utf-8" />
         <title><!-- Page title --></title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css"> -->
         <!-- <link rel="stylesheet" href="css/bootstrap.css" type="text/css"> -->
         <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script> -->
     </head>
