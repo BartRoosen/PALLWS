de26b3122fd008c66ab4039fd5df2d3fb3d99e1e
Bart Roosen <Bart Roosen>
2017-02-27 17:57:47 +0100

    corrected typo

--- src/Layers/Presentation/components/nav.php
+++ src/Layers/Presentation/components/nav.php
@@ -13,7 +13,7 @@
 		<li><a href="navcontroller.php?page=home" id='home'><span class="glyphicon glyphicon-home"></span></a></li>
 		<li><a href="navcontroller.php?page=pallzorg" id="pallzorg">Palliatieve zorg</a></li>
 		<li><a href="navcontroller.php?page=vrijwilligers" id="vrijwilligers">Vrijwilligers</a></li>
-		<li><a href="navcontroller.php?page=vrijwilligerworden" id="vrijwilligerworden">Vijwilliger worden</a></li>
+		<li><a href="navcontroller.php?page=vrijwilligerworden" id="vrijwilligerworden">Vrijwilliger worden</a></li>
 		<li><a href="navcontroller.php?page=getuigenissen" id="getuigenissen">Getuigenissen</a></li>
 		<li><a href="navcontroller.php?page=links" id="links">Links</a></li>
 		<li><a href="#" data-toggle="modal" data-target="#myModal" id="links">Leden</a></li>
