0e476d3b86ae4d48b0e908ff410aefa37a3e0712
Bart Roosen <Bart Roosen>
2017-03-31 09:21:45 +0200

    added .htaccess

--- eventcontroller.php
+++ eventcontroller.php
@@ -1,11 +1,5 @@
 <?php
-/*
-if($_SERVER["HTTPS"] != "on")
-{
-    header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
-    exit();
-}
-*/
+
 require_once("bootstrap.php");
 
 use Layers\Business\SessionHandler;
